d7d7c799dbeb3b04fa0097cea076dfe4b22cf7a5
fernandojrcontreras90-source <fernandojrcontreras90@gmail.com>
2025-09-01 21:01:19 -0700

    Delete analysis.py

--- analysis.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def analyze_deck(deck):
-    feedback = {}
-    for card, qty in deck.items():
-        # Assign random "bad card %" suggestion placeholder
-        change = random.randint(-10, 20)
-        feedback[card] = change
-    return feedback
